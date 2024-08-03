4fa9e9d8306f13e58146a7c93464e80a1ffd1c6e
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2024-08-03 17:44:51 +0100

    Deliveroo-clone native app first setup with screens final

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './(tabs)/HomeScreen';
+import HomeScreen from './(screens)/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
